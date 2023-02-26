78d8868c95633bfb0395e432e81d0025340860ab
MamaiOksana <jayko.mamayko@gmail.com>
2023-02-26 15:14:24 +0200

    add project

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Loader.css'
+
+const Loader = () => {
+    return (
+        <div>
+            <img className={'loader'} src={'https://cdn.pixabay.com/photo/2021/07/08/14/16/roll-film-6397021_960_720.png'}/>
+        </div>
+    );
+};
+
+export {
+    Loader
+} ;
